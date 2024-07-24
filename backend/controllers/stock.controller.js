e6c1edb6b48037a310a74481ee0879f4c7fcebeb
Evhiro <playingfrizz@gmail.com>
2024-07-24 10:55:56 +0800

    initital commit

---
+++ backend/controllers/stock.controller.js
@@ -0,0 +1,51 @@
+import { Stock } from "../models/stock.model.js"
+
+export async function getStocks(req, res){
+    try{
+        const stocks = await Stock.find();
+        return res.json(stocks);
+    }
+    catch(error){
+        console.log("getStock Controller Error", error.message);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+export async function postStock(req, res){
+    try{
+        const {
+            name, qty, invoice_no, desc, reciever, date, expiration_date, scan_copy, done_by, price
+        } = req.body;
+
+        if (!name || !qty || !desc || !invoice_no || !date || !expiration_date || !scan_copy || !done_by || !reciever || !price ){
+            return res.status(400).json({ message: false, message: "All fields are required" });
+        }
+
+        const newStock = new Stock({
+            name, qty, invoice_no, desc, reciever, date, expiration_date, scan_copy, done_by, price
+        });
+        
+        res.req(200).json({ success: true, message: "Entry successfully added" }) 
+    }
+    catch(error){
+        console.log("postStock Controller Error", error.message);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+export async function updateStock(req, res){
+    try{
+
+    }
+    catch(error){
+        console.log("updateStock Controller Error", error.message);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+export async function destroyStock(req, res){
+    try{
+
+    }
+    catch(error){
+        console.log("destroyStock Controller Error", error.message);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import { connectDB } from "./config/db.js";
+import { ENV_VARS } from "./config/envVars.js";
+import stockRoute from "./routes/stock.route.js";
+import authRoute from "./routes/auth.route.js";
+import express from "express";
+const app = express();
+const PORT = ENV_VARS.PORT;
+
+app.use(express.json());
+
+app.use("/api/v1/auth/", authRoute);
+app.use("/api/v1/stock/", stockRoute);
+
+app.listen(PORT, (error) => {
+    console.log('Server is running at http://localhost:' + PORT);
+    connectDB();
+});
+
