7a5b291d9309433b703e7f7b027fa24e2745b0f8
Evhiro <playingfrizz@gmail.com>
2024-08-08 15:45:20 +0800

    dont blame me(login page fixed)

--- frontend/src/layout/LandingLayout.jsx
+++ frontend/src/layout/LandingLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom"
 export const LandingLayout = () => {
   return (
-	<div className="min-h-screen h-full w-full">
+	<div className="min-h-screen h-full w-full grid">
 		<Outlet/>
 	</div>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,18 +18,19 @@ export const Login = () => {
     }
   };
   return (
-    <section className="grid grid-rows-[50px_1fr] md:grid-rows-1 md:grid-cols-2 dark:bg-slate-800 w-full h-full gap-5 p-5 md:p-0 text-center">
+    <section className="grid grid-rows-[50px_1fr] md:grid-rows-1 md:grid-cols-2  w-full h-full gap-5 p-5 md:p-0 text-center">
+      
       <div className=" bg-transparent md:bg-secondary  md:order-1 flex justify-center md:items-center relative ">
         <div>
-          <GiMedicines className=" text-6xl md:text-[10rem] text-accent" />
+          <GiMedicines className=" text-6xl md:text-[10rem] text-accent text-green-500" />
         </div>
         <div className="blurred p-5 absolute top-[2rem] left-28 right-28 bottom-[0%] md:top-[50%] md:bottom-[30%] md:left-0 md:right-0  "></div>
       </div>
 
-      <div className="mt-3 md:mt-0 self-start  flex flex-col justify-between p-0 md:p-5 h-full ">
+      <div className="mt-3 md:mt-0 self-start  flex flex-col justify-between p-0 md:p-5 h-full">
 
         <div className=" opacity-0 md:opacity-100 flex flex-row items-center gap-1">
-          <GoDotFill className=" text-accent text-md" />
+          <GoDotFill className=" text-accent text-md text-green-400" />
           <p className="  self-start font-semibold text-[0.8rem] ">
             Eurasia Research Pharma Corp
           </p>
@@ -61,9 +62,9 @@ export const Login = () => {
               <Links
                 to="forgetpass"
                 name="Forget Password"
-                style="self-end text-[0.8rem] text-accent font-semibold border-b-[1px] border-gray-400 "
+                style="self-end text-[0.8rem] text-accent font-semibold border-b-[1px] border-gray-400 dark:text-green-500 text-green-500"
               />
-              <button type="submit" className="dark:bg-green-500 bg-accent p-2 text-white rounded-md font-bold" disabled={isLoggingIn}>
+              <button type="submit" className="dark:bg-green-500 bg-green-500 bg-accent p-2 text-white rounded-md font-bold" disabled={isLoggingIn}>
                 {isLoggingIn ? 'Logging in...' : 'Login'}
               </button>
             </Form>
@@ -73,7 +74,7 @@ export const Login = () => {
             <Links
               to="sign-up"
               name=" Sign Up"
-              style=" text-accent dark:text-slate-400 font-bold border-b-[1px] border-gray-400  "
+              style=" text-accent dark:text-slate-400 font-bold border-b-[1px] border-gray-400 dark:text-green-500 text-green-500"
             />{" "}
           </p>
         </div>
