70563919005363e5fe41f24029d8508047876a66
Evhiro <playingfrizz@gmail.com>
2024-08-21 05:52:21 +0800

    hehe

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -103,7 +103,7 @@ const Sidebar = () => {
                     <li>
                     <a onClick={handleLogout} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <RiLogoutBoxLine className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" /> 
-                        <span className="flex-1 ms-3 text-start whitespace-nowrap">Log out</span>
+                        <span className="flex-1 ms-3 text-start whitespace-nowrap" disabled={isLoggingout}>{isLoggingout ? 'Logging Out...' : 'Log out'}</span>
                     </a>
                     </li>
                 </div>
